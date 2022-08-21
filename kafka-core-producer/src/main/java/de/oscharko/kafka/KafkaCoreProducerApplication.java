9939f1fc6598c3bf7934eaaf477078d9ce4a3f98
Oliver Scharkowski <o.scharkowski@oscharko.de>
2022-08-21 13:49:07 +0200

    Create project kafka-core-producer

---
+++ kafka-core-producer/src/main/java/de/oscharko/kafka/KafkaCoreProducerApplication.java
@@ -0,0 +1,13 @@
+package de.oscharko.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCoreProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCoreProducerApplication.class, args);
+	}
+
+}
